aea04827c278538cfe92c4919e2c8a820d724f57
Stephen-Wu-TVBS <stephenwu@tvbs.com.tw>
2024-05-29 10:37:52 +0800

    add test-id

--- tests/select.spec.ts
+++ tests/select.spec.ts
@@ -2,8 +2,10 @@ import { test, expect } from '@playwright/test';
 
 test('select visible', async ({ page }) => {
     await page.goto('http://127.0.0.1:5500/html/select.html');
-    // const combobox = page.getByRole('combobox');
-    const combobox = page.locator('id=cars');
+    const combobox = page.getByRole('combobox');
+    //const combobox = page.getByRole('option');
+    // const combobox = page.locator('id=cars');
+    //const combobox = page.getByTestId('123');
     await expect(combobox).toBeVisible();
     await combobox.selectOption('saab');
     await expect(combobox).toHaveValue('saab');
